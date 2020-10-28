5ec865453ef11ea4ada0699e0e7dedc5f4f948a9
PTKu <peter.kurhajec@inxton.com>
2020-10-28 11:38:48 +0100

    tests with recorder + clean up

---
+++ src/connectors/TcOpenConnector/pex/ComponentBase/fbComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using Vortex.Connector;
+
+namespace TcOpen
+{
+    public abstract partial class fbComponent
+    {     
+        partial void PexConstructor(IVortexObject parent, string readableTail, string symbolTail)
+        {
+            ComponentsCollector.Get.AddComponent(this);
+        }
+
+        internal virtual bool Simulate() { return true; }
+    }
+}
---
+++ src/connectors/TcOpenConnector/pex/Cylinders/fbCyclinder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace TcOpen
+{
+    public partial class fbCylinder
+    {
+        public int TimeToReachPosition { get; set; } = 500;
+
+        internal override bool Simulate()
+        {
+            if (this.outToHomePos.Cyclic)
+            {
+                this.inAtWorkPos.Synchron = false;
+                Thread.Sleep(TimeToReachPosition);
+                this.inAtHomePos.Synchron = true;
+            }
+                      
+            if (this.outToWorkPos.Cyclic)
+            {
+                this.inAtHomePos.Synchron = false;
+                Thread.Sleep(TimeToReachPosition);
+                this.inAtWorkPos.Synchron = true;
+            }
+                     
+            return true;
+        }
+    }
+}
