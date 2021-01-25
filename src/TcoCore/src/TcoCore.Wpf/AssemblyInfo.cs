c92e0e9bfa0ff496865612b2535896dee8e930ff
PTKu <p@nowhere.com>
2021-01-25 09:32:43 +0100

    minimize changes in files

--- src/TcoCore/src/TcoCore.Wpf/AssemblyInfo.cs
+++ src/TcoCore/src/TcoCore.Wpf/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Windows;
-using System.Reflection;
+using System.Windows;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
@@ -11,6 +10,3 @@ using System.Reflection;
 )]
 
 [assembly:Vortex.Presentation.Wpf.RenderableAssembly()]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-[assembly: AssemblyInformationalVersion("0.1.0-alpha.89+Branch.dev.Sha.09cd16228791dbc72c195243de74f8ce54d41d15")]
