fd009d8d43eaa20aab75a5923cfce32f38a41200
PTKu <p@nowhere.com>
2021-03-03 09:03:22 +0100

    simple manipulator example

---
+++ src/TcoApplicationExamples/PlcAppExamplesConnector/Entry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PlcAppExamples
+{
+    public static class Entry
+    {
+        const string amsId = "172.20.10.2.1.1";
+        const int amsPort = 851;
+
+        public static PlcAppExamples.PlcAppExamplesTwinController PlcAppExamples { get; }
+            = new PlcAppExamples.PlcAppExamplesTwinController(Vortex.Adapters.Connector.Tc3.Adapter.Tc3ConnectorAdapter.Create(amsId, amsPort, true));
+    }
+}
--- src/TcoCore/src/TcoCore.Wpf/TcoObject/TcoObjectView.xaml.cs
+++ src/TcoCore/src/TcoCore.Wpf/TcoObject/TcoObjectView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -23,6 +25,25 @@ namespace TcoCore
         public TcoObjectView()
         {
             InitializeComponent();
+        }
+    }
+
+    public class CountToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var list = value as IEnumerable<object>;
+            return list?.Count() > 0 ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
         }
     }
 }
