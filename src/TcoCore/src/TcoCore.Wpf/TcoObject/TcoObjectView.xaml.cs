0e1569d8c7021245ba602ceed0a711face228dca
PTKu <p@nowhere.com>
2021-03-04 17:31:00 +0100

    (examples) sequencers have separate instances of components

--- src/TcoCore/src/TcoCore.Wpf/TcoObject/TcoObjectView.xaml.cs
+++ src/TcoCore/src/TcoCore.Wpf/TcoObject/TcoObjectView.xaml.cs
@@ -65,4 +65,32 @@ namespace TcoCore
             return this;
         }
     }
+
+    public class BooleanToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var @is = (bool)value;
+                return @is ? Visibility.Visible : Visibility.Collapsed;
+            }
+            catch (Exception)
+            {
+                //!Swallow
+            }
+            
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
 }
