b164c0f39a9e4f943171ca1ea6d7af0791d58b83
PTKu <p@nowhere.com>
2021-03-04 10:43:02 +0100

    Object tree viewer moved to TcoCore.Wpf library Examples visulized with object tree viewer

---
+++ ivc.ps1
@@ -0,0 +1,4 @@
+.\_Vortex\builder\vortex.compiler.console.exe -s  .\src\TcoCore\TcoCore.slnf
+.\_Vortex\builder\vortex.compiler.console.exe -s .\src\TcoIoBeckhoff\TcoIoBeckhoff.slnf
+.\_Vortex\builder\vortex.compiler.console.exe -s .\src\TcoPneumatics\TcoPneumatics.slnf
+.\_Vortex\builder\vortex.compiler.console.exe -s .\src\TcoApplicationExamples\TcoApplicationExamples.slnf
