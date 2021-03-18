6a42d92f0108c0c2b35ae5168df53dfc99fe12af
MTS\peto.kurhajec <me@me.me>
2021-03-18 16:27:20 +0100

    rtc refactoring - start @TomKovac continues

--- src/TcoApplicationExamples/TcoApplicationExamples.Wpf/AssemblyInfo.cs
+++ src/TcoApplicationExamples/TcoApplicationExamples.Wpf/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Windows;
 
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
