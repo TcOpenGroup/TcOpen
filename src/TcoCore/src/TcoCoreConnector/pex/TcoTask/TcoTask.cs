35b4407b446d613eaafae6f4de09fd9fe64ddb44
PTKu <peter.kurhajec@inxton.com>
2021-04-21 11:16:32 +0200

    asp

--- src/TcoCore/src/TcoCoreConnector/pex/TcoTask/TcoTask.cs
+++ src/TcoCore/src/TcoCoreConnector/pex/TcoTask/TcoTask.cs
@@ -9,52 +9,19 @@ using System.Windows.Input;
 
 namespace TcoCore
 {
-    public partial class TcoTask : ICommand,INotifyPropertyChanged
-    {
-
-        private bool isEnabled;
+    public partial class TcoTask : ICommand
+    {        
         public event PropertyChangedEventHandler PropertyChanged;
         public event EventHandler CanExecuteChanged;
-
-        
-        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
-        public bool IsEnabled
-        {
-            get
-            {
-                isEnabled = this._enabled.Synchron;
-                NotifyPropertyChanged();
-                return isEnabled;
-            }
-            set
-            {
-                if (value != this.isEnabled)
-                {
-                    this.isEnabled = value;
-                    NotifyPropertyChanged();
-                }
-            }
-        }
-
-        
+                             
         public bool CanExecute(object parameter)
         {
-            //return IsEnabled;
-            return true;
+            return this._enabled.Cyclic;            
         }
 
         public void Execute(object parameter)
         {
             this._invokeRequest.Synchron = true;
         }
-
     }
-
 }
