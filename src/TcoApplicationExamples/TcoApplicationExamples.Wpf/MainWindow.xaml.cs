bfa7bab07069734e3fcccad91bee204e179c39de
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2021-04-21 09:39:53 +0200

    Remove unnecessary code

--- src/TcoApplicationExamples/TcoApplicationExamples.Wpf/MainWindow.xaml.cs
+++ src/TcoApplicationExamples/TcoApplicationExamples.Wpf/MainWindow.xaml.cs
@@ -1,36 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace TcoApplicationExamples.Wpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            //RenderableContentX.PresentationType = "Control";
-        }   
     }
 }
