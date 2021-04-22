e00bc5ad1c9f0c687bb62b83c9217d87fcbc1c16
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2021-04-22 11:53:07 +0200

    WIP

---
+++ src/TcoCore/src/TcoCore.Wpf/TcoSequencer/TcoSequencerModeController/TcoSequencerModeControllerView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TcoCore
+{
+    /// <summary>
+    /// Interaction logic for TcoTask.xaml
+    /// </summary>
+    public partial class TcoSequencerModeControllerView : UserControl
+    {
+        public TcoSequencerModeControllerView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/TcoCore/src/TcoCore.Wpf/TcoSequencer/TcoSequencerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Vortex.Presentation.Wpf;
+
+namespace TcoCore
+{
+    public class TcoSequencerViewModel : RenderableViewModel
+    {
+        public TcoSequencerViewModel()
+        {
+            
+        }
+
+        public TcoSequencer TcoSequencer { get; private set; }
+
+        public override object Model { get => TcoSequencer; set { TcoSequencer = value as TcoSequencer; } }
+
+        
+    }
+}
