1bd4db703c30d8041715b724c62ce4894854185e
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2021-04-22 15:55:29 +0200

    wip

---
+++ src/TcoApplicationExamples/TcoApplicationExamples.Wpf/Tutorial/101_Color_sequencer/Color_ExampleView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Controls;
+using Vortex.Presentation.Wpf;
+
+namespace PlcAppExamples
+{
+    public partial class Color_ExampleView : UserControl
+    {
+        public Color_ExampleView()
+        {
+            InitializeComponent();
+        }
+    }
+    public class Color_ExampleViewModel : RenderableViewModel
+    {
+        public Color_ExampleViewModel()
+        {
+
+        }
+
+        public Color_Example Color_Example { get; set; }
+
+        public override object Model { get => Color_Example; set => Color_Example = value as Color_Example; }
+    }
+}
