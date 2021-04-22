12ee28aa9477eebdbb5ae80d6108c47264e1e2c5
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2021-04-22 13:39:53 +0200

    Added sequencer view

--- src/TcoCore/src/TcoCore.Wpf/TcoSequencer/TcoSequencerModeController/TcoSequencerModeControllerView.xaml.cs
+++ src/TcoCore/src/TcoCore.Wpf/TcoSequencer/TcoSequencerModeController/TcoSequencerModeControllerView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using Vortex.Presentation.Wpf.Converters;
 
 namespace TcoCore
 {
@@ -10,6 +14,19 @@ namespace TcoCore
         public TcoSequencerModeControllerView()
         {
             InitializeComponent();
+        }
+    }
+
+    public class SequenceModeVisibleInConverter : BaseConverter
+    {
+
+        public override object ToConvert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            eSequencerMode VisibleInMode = eSequencerMode.Invalid;
+            Enum.TryParse((string)parameter, true, out VisibleInMode);
+            
+            var enumValue = (eSequencerMode)Enum.ToObject(typeof(eSequencerMode), value);
+            return enumValue == VisibleInMode ? Visibility.Visible : Visibility.Collapsed;
         }
     }
 }
