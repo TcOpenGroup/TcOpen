648d8be26484a68331d68801fda48f96b18a338a
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2021-04-23 14:42:48 +0200

    Task execution 302

---
+++ src/TcoApplicationExamples/TcoApplicationExamples.Wpf/Tutorial/302_TaskExecution/TaskExecution302.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PlcAppExamples
+{
+    public partial class TaskExecution302 : UserControl
+    {
+        public TaskExecution302()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+
+}
