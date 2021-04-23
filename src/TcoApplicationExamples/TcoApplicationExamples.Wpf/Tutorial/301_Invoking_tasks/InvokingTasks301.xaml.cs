9d02b50c0c4496f4a7bc0a15870c019f4a84edc9
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2021-04-23 12:53:25 +0200

    301 - Invoking tasks to run sequences.

---
+++ src/TcoApplicationExamples/TcoApplicationExamples.Wpf/Tutorial/301_Invoking_tasks/InvokingTasks301.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PlcAppExamples
+{
+    public partial class InvokingTasks301 : UserControl
+    {
+        public InvokingTasks301()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+
+}
