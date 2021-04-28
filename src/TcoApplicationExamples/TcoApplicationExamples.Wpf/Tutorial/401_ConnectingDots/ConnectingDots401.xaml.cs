065a2da0b7577ee28d55cfe0bec225d14bd54cb8
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2021-04-28 14:33:19 +0200

    Autoupdate messages hotfix

--- src/TcoApplicationExamples/TcoApplicationExamples.Wpf/Tutorial/401_ConnectingDots/ConnectingDots401.xaml.cs
+++ src/TcoApplicationExamples/TcoApplicationExamples.Wpf/Tutorial/401_ConnectingDots/ConnectingDots401.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows.Controls;
+using Vortex.Presentation.Wpf;
+using System.Linq;
+using TcoCore;
 
 namespace PlcAppExamples
 {
@@ -9,8 +12,30 @@ namespace PlcAppExamples
             InitializeComponent();
         }
 
+        private void OnRendered(object sender, System.Windows.SizeChangedEventArgs e)
+        {
+            var diagViewModel = sender
+                 .As<RenderableContentControl>()
+                 .Content
+                 .As<Panel>()
+                 .Children[0]
+                 .As<ContentControl>()
+                 .Content
+                 .As<ContentControl>()
+                 .DataContext
+                 .As<TcoContextDiagnosticsViewModel>();
 
+            if (SubscirbeOnce)
+            {
+                SubscirbeOnce = false;
+                Entry.PlcAppExamples.MAIN_PRG._TcOpenTutorial.Station001._sequence._currentStep.ID.Subscribe((a, b) => diagViewModel.UpdateMessagesCommand.Execute(null));
+            }
+        }
+        public bool SubscirbeOnce { get; set; } = true;
     }
 
-
+    public static class Ext
+    {
+        public static T As<T>(this object @object) where T : class => (@object as T);
+    }
 }
