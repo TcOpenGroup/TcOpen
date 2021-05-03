8a292748daf0afeec7ccea8016467a503e7d5053
PTKu <me@me.me>
2021-05-03 08:56:15 +0200

    removed ivc script - replaced with modified behaviour of IVC CLI

--- ivc.ps1
+++
@@ -1,4 +0,0 @@
-.\_Vortex\builder\vortex.compiler.console.exe -s  .\src\TcoCore\TcoCore.slnf
-.\_Vortex\builder\vortex.compiler.console.exe -s .\src\TcoIoBeckhoff\TcoIoBeckhoff.slnf
-.\_Vortex\builder\vortex.compiler.console.exe -s .\src\TcoPneumatics\TcoPneumatics.slnf
-.\_Vortex\builder\vortex.compiler.console.exe -s .\src\TcoApplicationExamples\TcoApplicationExamples.slnf
