41fefd13917de3bdbfa842a2cd898228fa80e388
PTKu <me@me.me>
2021-05-04 08:24:36 +0200

    library template + script

---
+++ src/librarytemplate/tests/Sandbox.PlcTemplate.Wpf/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PlcTemplate;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Vortex.Connector;
+using Vortex.Connector.ValueTypes;
+using System.Linq;
+
+namespace Sandbox.PlcTemplate.Wpf
+{    
+    public class MainWindowViewModel 
+    {
+        public MainWindowViewModel()
+        {            
+        
+        }
+        
+        public PlcTemplateTwinController PlcTemplatePlc { get; } = Entry.PlcTemplatePlc;       
+    }
+}
