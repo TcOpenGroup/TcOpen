d6a1739199b8b77f655770165429c909d694b738
Tomas Kovac <tomas.kovac@mts.sk>
2021-05-14 13:38:28 +0200

    some simple drive wpf visu

---
+++ src/TcoDrivesBeckhoff/src/TcoDrivesBeckhoff.Wpf/TcoDriveSimple/BoolToVisibilityConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using Vortex.Presentation.Wpf.Converters;
+
+namespace TcoDrivesBeckhoff
+{
+    public class BoolToVisibilityConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                bool _value = (bool)value;
+                if (_value)
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+            catch (Exception)
+            {
+                // Swallow
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+}
