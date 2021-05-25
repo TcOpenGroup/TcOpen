0e0d51389ce8021b5c4b84f4dff68b47490a59d7
TomKovac <61820360+TomKovac@users.noreply.github.com>
2021-05-25 16:23:38 +0200

    Feature/beckhoff drives (#88)
    * some simple drive wpf visu
    
    * TcoTaskView enabled issue found
    
    * TcoDriveSimple with manual power override
    
    * build script closes all instance of VS before opening a fresh one
    
    * Fixed typo in build script
    
    * Try-catch on closing 'devenv' to ignore error
    
    * Update build.ps1
    
    * conflicts resolution
    
    * post merge fixies
    
    * fix: added missing project file in solution folder
    
    * sync
    
    * TcoDriveSimple first tests added
    
    Co-authored-by: Tomas Kovac <tomas.kovac@mts.sk>
    Co-authored-by: PTKu <PTKu>
    Co-authored-by: Peter <61538034+PTKu@users.noreply.github.com>

---
+++ src/TcoDrivesBeckhoff/src/TcoDrivesBeckhoff.Wpf/TcoDriveSimple/VisibleOnBool.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Vortex.Presentation.Wpf.Converters;
+
+namespace TcoDrivesBeckhoff
+{
+    public class VisibleOnBool      : BaseConverter
+    {
+        public override object ToConvert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                bool _value = (bool)value;
+                bool _shouldBvisible;
+                Boolean.TryParse(parameter.ToString(), out _shouldBvisible);
+
+                if(_value == _shouldBvisible)
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+            catch
+            {
+                //swallow
+                return Visibility.Collapsed;
+            }
+        }
+    }
+}
+
+
