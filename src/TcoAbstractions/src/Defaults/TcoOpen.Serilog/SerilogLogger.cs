df370466078fb3a4e3c00102f9f0116acb38107d
PTKu <61538034+PTKu@users.noreply.github.com>
2021-05-25 16:01:02 +0200

    next time amend

---
+++ src/TcoAbstractions/src/Defaults/TcoOpen.Serilog/SerilogLogger.cs
@@ -0,0 +1,54 @@
+﻿using Serilog;
+using System;
+using TcOpen.Abstractions.Logging;
+using Serilog.Formatting.Compact;
+namespace TcoOpen.Logging
+{
+    public class SerilogLogger : ITcoLogger
+    {
+        public SerilogLogger(LoggerConfiguration configuration)
+        {
+            logger = configuration.CreateLogger();
+        }
+
+        public SerilogLogger()
+        {
+            logger = new LoggerConfiguration()
+                                    .WriteTo
+                                    .Console()                                                                        
+                                    .CreateLogger();
+        }
+
+        private readonly Serilog.ILogger logger;
+
+        public void Debug<T>(string stringTemplate, T payload = default)
+        {
+            logger.Debug<T>(stringTemplate, payload);
+        }
+
+        public void Verbose<T>(string stringTemplate, T payload = default)
+        {
+            logger.Verbose<T>(stringTemplate, payload);
+        }
+
+        public void Information<T>(string stringTemplate, T payload = default)
+        {
+            logger.Information<T>(stringTemplate, payload);
+        }
+
+        public void Warning<T>(string stringTemplate, T payload = default)
+        {
+            logger.Warning<T>(stringTemplate, payload);
+        }
+
+        public void Error<T>(string stringTemplate, T payload = default)
+        {
+            logger.Error<T>(stringTemplate, payload);
+        }
+
+        public void Fatal<T>(string stringTemplate, T payload = default)
+        {
+            logger.Fatal<T>(stringTemplate, payload);
+        }
+    }
+}
---
+++ src/TcoAbstractions/src/TcOpen.Abstractions/Logging/ILogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcOpen.Abstractions.Logging
+{
+    public interface ITcoLogger
+    {
+        void Debug<T>(string stringTemplate, T payload = default(T));
+        void Verbose<T>(string stringTemplate, T payload = default(T));
+        void Information<T>(string stringTemplate, T payload = default(T));
+        void Warning<T>(string stringTemplate, T payload = default(T));
+        void Error<T>(string stringTemplate, T payload = default(T));
+        void Fatal<T>(string stringTemplate, T payload = default(T));
+
+    }    
+}
