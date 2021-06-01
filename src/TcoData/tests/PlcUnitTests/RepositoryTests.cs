fc2db16fc583f13a6ecb0e52a011e53a8813bede
Jokinko <jozefchmelar@users.noreply.github.com>
2021-06-01 10:07:10 +0200

    Remote task, data task and CRUD operations on PLC data

--- src/TcoData/tests/PlcUnitTests/RepositoryTests.cs
+++ src/TcoData/tests/PlcUnitTests/RepositoryTests.cs
@@ -7,11 +7,12 @@ using TcoData.Repository.Json;
 using System.Linq;
 using System;
 using System.IO;
+using TcoCore;
 
 namespace TcoDataUnitTests
 {
     [TestFixture, Timeout(5000)]
-    public class fbUnitTestExampleTests
+    public class RepositoryTests
     {
         TestDataContext sut;
         public IRepository<PlainstProcessData> Repository { get; set; }
@@ -28,6 +29,7 @@ namespace TcoDataUnitTests
             ClearFolder(DataFolder);
             sut.DataTests.DataManager.InitializeRepository<PlainstProcessData>(Repository as IRepository);
             Entry.TcoDataTests.Connector.BuildAndStart().ReadWriteCycleDelay = 100;
+            sut.ExecuteProbeRun(10, (int)eDataTests.RestoreTasks);
         }
 
         private void ClearFolder(string dataDir)
@@ -37,19 +39,11 @@ namespace TcoDataUnitTests
                 File.Delete(file);
             }
         }
-         
 
-        [Test, Order((int)eDataTests.RestoreTasks)]
-        public void Restore()
-        {
-            //-- Act
-            sut.ExecuteProbeRun(
-                counts : 10,
-                testId: (int)eDataTests.RestoreTasks
-          );
-        }
+        [SetUp]
+        public void RunTwoCycles() => sut.ExecuteProbeRun(2, 0);
 
-            [Test, Order((int)eDataTests.CreateData)]
+        [Test, Order((int)eDataTests.CreateData)]
         public void Recored_with_id_will_be_created()
         {
             //-- Arrange
@@ -60,7 +54,7 @@ namespace TcoDataUnitTests
             //-- Act
             sut.ExecuteProbeRun(
                 testId: (int)eDataTests.CreateData,
-                endCondition: () => sut._createDataDone.Cyclic);
+                endCondition: () => sut._createDataDone.Synchron);
             var record = Repository.GetRecords(identifier).First();
             //-- Assert
             Assert.IsNotNull(record);
@@ -80,7 +74,7 @@ namespace TcoDataUnitTests
             //-- Act
             sut.ExecuteProbeRun(
                 testId: (int)eDataTests.UpdateData,
-                endCondition: () => sut._updateDataDone.Cyclic);
+                endCondition: () => sut._updateDataDone.Synchron);
             var record = Repository.GetRecords(identifier).First();
             //-- Assert
             Assert.IsNotNull(record);
@@ -119,7 +113,8 @@ namespace TcoDataUnitTests
             sut._identifier.Synchron = data._Id;
             sut.ExecuteProbeRun(
                 testId: (int)eDataTests.ReadData,
-                endCondition: () => sut._readDataDone.Cyclic);
+                endCondition: () => sut._readDataDone.Synchron);
+
             //-- Assert
             Assert.AreEqual(data._Id, sut.DataTests.DataManager._data._Id.Synchron);
             Assert.AreEqual(data.Cu_1.A_Check_1.ProcessedData.FailureDescription, sut.DataTests.DataManager._data.Cu_1.A_Check_1.ProcessedData.FailureDescription.Synchron);
@@ -134,16 +129,84 @@ namespace TcoDataUnitTests
             var identifier = "some-id-123";
             sut._identifier.Synchron = identifier;
             sut._deleteDataDone.Synchron = false;
+
             //-- Act
             var record = Repository.GetRecords(identifier).First();
 
             sut.ExecuteProbeRun(
                 testId: (int)eDataTests.DeleteData,
-                endCondition: () => sut._deleteDataDone.Cyclic);
+                endCondition: () => sut._deleteDataDone.Synchron);
 
             record = Repository.GetRecords(identifier).FirstOrDefault();
+
             //-- Assert
             Assert.IsNull(record);
+        }
+
+        [Test, Order((int)eDataTests.UpdateDataWithoutChange)]
+        public void Update_data_without_change_works()
+        {
+            //-- Arrange
+            var identifier = "some-id-123456";
+            sut._identifier.Synchron = identifier;
+            sut._updateDataWithoutChangeDone.Synchron = false;
+
+            //-- Act
+            sut.DataTests.DataManager._data.Cu_1.A_Check_1.ProcessedData.FailureDescription.Synchron = nameof(Update_data_without_change_works);
+            sut.ExecuteProbeRun(
+                testId: (int)eDataTests.UpdateDataWithoutChange,
+                endCondition: () => sut._updateDataWithoutChangeDone.Synchron);
+
+            var record = Repository.GetRecords(identifier).First();
+
+            //-- Assert
+            Assert.IsNotNull(record);
+            Assert.AreEqual(nameof(Update_data_without_change_works), record.Cu_1.A_Check_1.ProcessedData.FailureDescription);
+        }
+
+
+        [Test, Order((int)eDataTests.UpdateDataWithInvalidId)]
+        public void Update_data_with_invalid_id_will_report_the_issue()
+        {
+            //-- Arrange
+            sut._updateDataWithInvalidIdDone.Synchron = false;
+
+            //-- Act
+            sut.ExecuteProbeRun(
+                testId: (int)eDataTests.UpdateDataWithInvalidId,
+                endCondition: () => sut._updateDataWithInvalidIdDone.Synchron);
+
+            //-- Assert
+            Assert.True(sut.DataTests.DataManager._updateTask._hasException.Synchron);
+        }
+
+        [Test, Order((int)eDataTests.ReadDataWithInvlaidId)]
+        public void Read_data_with_invalid_id_will_report_the_issue()
+        {
+            //-- Arrange
+            sut._readDataWithInvlaidIdDone.Synchron = false;
+            //-- Act
+            sut.ExecuteProbeRun(
+                testId: (int)eDataTests.ReadDataWithInvlaidId,
+                endCondition: () => sut._readDataWithInvlaidIdDone.Synchron);
+
+            //-- Assert
+            Assert.True(sut.DataTests.DataManager._readTask._hasException.Synchron);
+        }
+
+        [Test, Order((int)eDataTests.DeleteDataWithInvalidId)]
+        public void Delete_data_with_invalid_id_will_report_the_issue()
+        {
+            //-- Arrange
+            sut._deleteDataWithInvalidIdDone.Synchron = false;
+            //-- Act
+            sut.ExecuteProbeRun(
+                testId: (int)eDataTests.DeleteDataWithInvalidId,
+                endCondition: () => sut._deleteDataWithInvalidIdDone.Synchron);
+
+            //-- Assert
+            Assert.False(sut.DataTests.DataManager._deleteTask._hasException.Synchron);
+            Assert.True(sut.DataTests.DataManager._deleteTask._taskState.Synchron == (int) eTaskState.Done);
         }
 
     }
