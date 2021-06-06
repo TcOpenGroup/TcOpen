d1b6cda11300ec8daf1db958f0d0fe29189b49d7
Dejan Milicic <dejan.milicic@gmail.com>
2021-06-06 20:02:27 +0200

    RavenDB repository project setup

---
+++ src/TcoData/src/Repository/RavenDb/TcOpen.Inxton.RavenDb/Raven/RavenDbRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TcOpen.Inxton.Abstractions.Data;
+using TcOpen.Inxton.Data;
+
+namespace TcOpen.Inxton.RavenDb.Raven
+{
+    public class RavenDbRepository<T> : RepositoryBase<T>
+        where T : IBrowsableDataObject
+    {
+        protected override void CreateNvi(string identifier, T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override T ReadNvi(string identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void UpdateNvi(string identifier, T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DeleteNvi(string identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override long CountNvi { get; }
+
+        protected override IEnumerable<T> GetRecordsNvi(string identifierContent, int limit, int skip)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override long FilteredCountNvi(string identifierContent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IQueryable<T> Queryable { get; }
+    }
+}
