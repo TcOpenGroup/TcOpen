e44844773d23d6b4f7d1c3668450d504963a27ff
Dejan Milicic <dejan.milicic@gmail.com>
2021-06-14 23:29:23 +0200

    Clean database before and after tests

--- src/TcoData/tests/TcoData.Repository.Integration.Tests/Repository/RavenDbRepositoryTest.cs
+++ src/TcoData/tests/TcoData.Repository.Integration.Tests/Repository/RavenDbRepositoryTest.cs
@@ -1,42 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TcOpen.Inxton.RavenDb;
+﻿using TcOpen.Inxton.RavenDb;
 
 namespace TcoDataUnitTests
 {
     public class RavenDbRepositoryTest : RepositoryBaseTests
     {
-        public override void Init()
-        {
-            if (this.repository == null)
-            {
-                var parameters = new RavenDbTestRepositorySettings<DataTestObject>();
-                var parametersAltered = new RavenDbTestRepositorySettings<DataTestObjectAlteredStructure>();
-                //var parameters = new RavenDbRepositorySettings<DataTestObject>(new string[]{"http://localhost:8080"}, "TestDataBase", "", "credentials");
-                //var parametersAltered = new RavenDbRepositorySettings<DataTestObjectAlteredStructure>(new string[] { "http://localhost:8080" }, "TestDataBase", "", "");
-                
-                this.repository = new RavenDbRepository<DataTestObject>(parameters);
-                this.repository_altered_structure = new RavenDbRepository<DataTestObjectAlteredStructure>(parametersAltered);
-            }
-
-            foreach (var item in this.repository.GetRecords("*"))
-            {
-                repository.Delete(item._EntityId);
-            }
-        }
-
-        public override void TearDown()
+        private void CleanDatabase()
         {
             if (this.repository != null)
             {
                 foreach (var item in this.repository.GetRecords("*"))
-                {
                     repository.Delete(item._EntityId);
-                }
             }
+
+            if (this.repository_altered_structure != null)
+            {
+                foreach (var item in this.repository_altered_structure.GetRecords("*"))
+                    repository_altered_structure.Delete(item._EntityId);
+            }
+        }
+
+        public override void Init()
+        {
+            if (this.repository == null)
+            {
+                //var parameters = new RavenDbTestRepositorySettings<DataTestObject>();
+                //var parametersAltered = new RavenDbTestRepositorySettings<DataTestObjectAlteredStructure>();
+                var parameters = new RavenDbRepositorySettings<DataTestObject>(new string[]{"http://localhost:8080"}, "TestDataBase", "", "credentials");
+                var parametersAltered = new RavenDbRepositorySettings<DataTestObjectAlteredStructure>(new string[] { "http://localhost:8080" }, "TestDataBase", "", "");
+                
+                this.repository = new RavenDbRepository<DataTestObject>(parameters);
+                this.repository_altered_structure = new RavenDbRepository<DataTestObjectAlteredStructure>(parametersAltered);
+            }
+            
+            CleanDatabase();
+        }
+
+        public override void TearDown()
+        {
+            CleanDatabase();
 
             base.TearDown();
         }
