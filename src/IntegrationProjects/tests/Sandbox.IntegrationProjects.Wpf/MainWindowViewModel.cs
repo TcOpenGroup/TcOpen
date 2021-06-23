594c39de62b3d2fa65a385ba7105026213dd3262
PTKu <61538034+PTKu@users.noreply.github.com>
2021-06-23 10:59:25 +0200

    integration project clean up

--- src/IntegrationProjects/tests/Sandbox.IntegrationProjects.Wpf/MainWindowViewModel.cs
+++ src/IntegrationProjects/tests/Sandbox.IntegrationProjects.Wpf/MainWindowViewModel.cs
@@ -5,16 +5,20 @@ using System.ComponentModel;
 using Vortex.Connector;
 using Vortex.Connector.ValueTypes;
 using System.Linq;
+using Vortex.Presentation.Wpf;
+using TcOpen.Inxton.Swift.Wpf;
 
 namespace Sandbox.IntegrationProjects.Wpf
 {    
-    public class MainWindowViewModel 
+    public class MainWindowViewModel : BindableBase
     {
         public MainWindowViewModel()
-        {            
-        
+        {
+            SwiftRecordVm = new SwiftRecorderViewModel(IntegrationProjectsPlc.MAIN_TECHNOLOGY._technology._ST001);
         }
-        
+
+        public SwiftRecorderViewModel SwiftRecordVm { get; }
+
         public IntegrationProjectsTwinController IntegrationProjectsPlc { get; } = Entry.IntegrationProjectsPlc;       
     }
 }
