60fc21e9b0baa233035e0f10585883f9ea76fdb9
PTKu <61538034+PTKu@users.noreply.github.com>
2021-06-24 21:46:11 +0200

    - added copy of the TcOpen.Hammer into this repo (with project references instead of nugets) - solved an issue when UI may dead lock due to connector operations when batch reading connector

---
+++ src/TcOpen.Hammer/HMI/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using PlcHammer;
+using PlcHammerConnector;
+using System;
+using System.Linq;
+using System.Windows;
+using TcOpen.Inxton.MongoDb;
+
+namespace HMI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Creates new instance of this application.
+        /// </summary>
+        public App()
+        {
+            // App setup
+            TcOpen.Inxton.TcoAppDomain.Current.Builder
+                .SetUpLogger(new TcOpen.Inxton.Logging.SerilogAdapter()) // Sets the logger configuration (default reports only to console).
+                .SetDispatcher(TcoCore.Wpf.Threading.Dispatcher.Get);    // This is necessary for UI operation.
+
+            // Execute PLC connector operations.
+            Entry.PlcHammer.Connector.ReadWriteCycleDelay = 50; // Cyclical access period.
+            
+            Entry.PlcHammer.Connector.BuildAndStart(); // Create connection, loads symbols, and fires cyclic operations.
+
+            SetUpDatabase();
+        }
+
+        private static void SetUpDatabase()
+        {
+            var mongoUri = "mongodb://localhost:27017";
+            var databaseName = "Hammer";
+            var collectionName = "HammerCollection";
+
+            var processDataRepositorySettings = new MongoDbRepositorySettings<PlainStation001_ProductionData>(mongoUri, databaseName, collectionName);
+            var processDataRepository = new MongoDbRepository<PlainStation001_ProductionData>(processDataRepositorySettings);
+            
+            Entry.PlcHammer.MAIN._app._station001._processDataManager.InitializeRepository(processDataRepository);
+
+            Entry.PlcHammer.MAIN._app._station001._technologicalDataManager
+                .InitializeRepository(new MongoDbRepository<PlainStation001_TechnologicalSettings>
+                                     (new MongoDbRepositorySettings<PlainStation001_TechnologicalSettings>(mongoUri, databaseName, "TechnologicalSettings")));
+        }
+    }
+}
---
+++ src/TcOpen.Hammer/HMI/Components/Drive/DriveSimServiceViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vortex.Presentation.Wpf;
+
+namespace PlcHammer
+{
+    public class DriveSimServiceViewModel : RenderableViewModel
+    {
+        public DriveSimServiceViewModel()
+        {
+           
+        }
+
+
+        public PlainDriveSim GetLogPayload()
+        {
+            var plain = this.Component.CreatePlainerType();
+            this.Component.FlushOnlineToPlain(plain);
+            return plain;
+        }
+
+        public DriveSim Component { get; private set; }
+
+        public override object Model { get { return Component; } set { Component = value as DriveSim; Component._moveAbsoluteTask.LogPayloadDecoration = () => GetLogPayload(); } }
+    }
+}
