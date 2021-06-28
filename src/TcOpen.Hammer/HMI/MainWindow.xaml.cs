b018523f4f576538785bc9f0ab9eddbe1a62e36f
PTKu <61538034+PTKu@users.noreply.github.com>
2021-06-28 13:56:23 +0200

    example visu improvements

--- src/TcOpen.Hammer/HMI/MainWindow.xaml.cs
+++ src/TcOpen.Hammer/HMI/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace PlcHammer.Hmi
         {
             var verticalPosition = (short)value;
 
-            return verticalPosition - 90;          
+            return verticalPosition < 50 ? 50 : verticalPosition;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
@@ -39,6 +39,51 @@ namespace PlcHammer.Hmi
             return this;
         }
     }
+
+    public class VerticalPistonPositionConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var verticalPosition = (short)value;
+
+            return verticalPosition < 50 ? 50 : verticalPosition;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+
+ 
+
+    public class AxisPositionConverter : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var horizontalPisition = (double)value;
+
+            return horizontalPisition >= 640 ? 640 : horizontalPisition <= 0 ? 0 : horizontalPisition;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+
+
+
 
     public class OK_NOK_Converter : MarkupExtension, IValueConverter
     {
