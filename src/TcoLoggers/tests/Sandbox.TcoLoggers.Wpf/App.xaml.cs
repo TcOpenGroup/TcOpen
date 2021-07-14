6683fa7c9e1c489e0ed8547aada7e8daf4b7b78e
PTKu <me@me.me>
2021-07-14 12:05:21 +0200

    TcoLoggers moved from TcoCore to separate library

---
+++ src/TcoLoggers/tests/Sandbox.TcoLoggers.Wpf/App.xaml.cs
@@ -0,0 +1,26 @@
+using TcoLoggers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sandbox.TcoLoggers.Wpf
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            TcOpen.Inxton.TcoAppDomain.Current.Builder
+                .SetUpLogger(new TcOpen.Inxton.Logging.SerilogAdapter())
+                .SetDispatcher(TcoCore.Wpf.Threading.Dispatcher.Get);
+            
+            Entry.TcoLoggersPlc.Connector.BuildAndStart().ReadWriteCycleDelay = 75;
+        }
+    }
+}
