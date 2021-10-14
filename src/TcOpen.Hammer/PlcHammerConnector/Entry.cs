bfb6f516afbbaad8e1f5181f084439a3ecab20f7
Roman <roman.grabichler@gema-technik.de>
2021-10-14 16:01:22 +0200

    docker dump mongo

--- src/TcOpen.Hammer/PlcHammerConnector/Entry.cs
+++ src/TcOpen.Hammer/PlcHammerConnector/Entry.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using Vortex.Adapters.Connector.Tc3.Adapter;
+﻿using Vortex.Adapters.Connector.Tc3.Adapter;
 
 namespace PlcHammerConnector
 {
     public class Entry
     {
-        public readonly static string AmsId = Environment.GetEnvironmentVariable("Tc3Target");
+        public static readonly string AmsId = "192.168.3.23.1.1";
 
-        public static PlcHammer.PlcHammerTwinController PlcHammer { get; } 
+        public static PlcHammer.PlcHammerTwinController PlcHammer { get; }
             = new PlcHammer.PlcHammerTwinController(Tc3ConnectorAdapter.Create(AmsId, 851, true));
     }
 }
