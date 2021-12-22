e973f980eb78edc7cc82c67b56172b9b76bee1e7
Jozef Chmelar <jozef.chmelar.ml@mts.sk>
2021-12-22 11:58:43 +0100

    init work on mqtt

---
+++ src/TcOpen.Inxton/src/TcOpen.Inxton.Mqtt/TcOpen.Inxton.Mqtt/MqttClient.cs
@@ -0,0 +1,45 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Publishing;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Vortex.Connector;
+
+namespace TcOpen.Inxton.Mqtt
+{
+    public interface IStringPayloadFormatter
+    {
+        string Format(object plain);
+    }
+
+    public class JsonStringPayloadFormatter : IStringPayloadFormatter
+    {
+        public string Format(object plain) => JsonConvert.SerializeObject(plain);
+    }
+    public class MqttClientxx : IDisposable
+    {
+        public IMqttClient Client { get; }
+        public IStringPayloadFormatter PayloadFormatter { get; set; } = new JsonStringPayloadFormatter();
+
+        private IDictionary<string, Action<string>> TopicHooks;
+        public MqttClientxx(IMqttClient Client)
+        {
+            this.Client = Client;
+            Client.UseApplicationMessageReceivedHandler((msg) => TopicHooks[msg.ApplicationMessage.Topic](msg.ApplicationMessage.ConvertPayloadToString()));
+        }
+        public Task<MqttClientPublishResult> PublishAsync(object plain, string topic)
+        {
+            return Client.PublishAsync(topic, PayloadFormatter.Format(plain));
+        }
+
+        public void Subsribe(string topic, Action<string> OnMessage)
+        {
+            Client.SubscribeAsync(topic);
+            TopicHooks[topic] = OnMessage;
+        }
+        public void Dispose() => Client.Dispose();
+    }
+}
---
+++ src/TcOpen.Inxton/tests/TcOpen.Inxton.MqttTests/LocalMqttTest.cs
@@ -0,0 +1,69 @@
+﻿using MQTTnet;
+using MQTTnet.Client.Receiving;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TcOpen.Inxton.Mqtt;
+
+namespace TcOpen.Inxton.Logging.Tests
+{
+
+    class RelayHandler : IMqttApplicationMessageReceivedHandler
+    {
+        public Func<MqttApplicationMessageReceivedEventArgs, string> OnMessage;
+        public bool MessageNotDelivered { get; private set; } = true;
+
+        public RelayHandler(Func<MqttApplicationMessageReceivedEventArgs, string> onMessage)
+        {
+            OnMessage = onMessage;
+        }
+
+        public Task HandleApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs eventArgs)
+        {
+            if (MessageNotDelivered)
+            {
+                MessageNotDelivered = false;
+                return Task.FromResult(OnMessage(eventArgs));
+            }
+            else
+                throw new Exception("Only one message per handler");
+        }
+
+
+    }
+    [TestFixture]
+    public class LocalMqttTest
+    {
+        [Test]
+        public void DummyLoggerTest()
+        {
+            //arrange
+            var mqttFactory = new MqttFactory();
+            var port = 1337;
+            var mqttServer = mqttFactory.CreateMqttServer();
+            var mqttServerOptions = mqttFactory
+                    .CreateServerOptionsBuilder()
+                    .WithClientId("clientId")
+                    .WithDefaultEndpointPort(port)
+                    .Build();
+
+            var mqttClient = mqttFactory.CreateMqttClient();
+            var mqttClientOptions = mqttFactory.CreateClientOptionsBuilder().WithTcpServer("localhost", port).Build();
+
+            mqttServer.StartAsync(mqttServerOptions);
+            mqttClient.ConnectAsync(mqttClientOptions, System.Threading.CancellationToken.None);
+            var mqtt = new MqttClientxx(mqttClient);
+            //act
+            string messg = "";
+            var messageHandler = new RelayHandler((msg) => messg = msg.ApplicationMessage.ConvertPayloadToString());
+            mqttServer.ApplicationMessageReceivedHandler = messageHandler;
+            mqtt.PublishAsync(new { a = "bitch" }, "/topic").Wait();
+            while (messageHandler.MessageNotDelivered) { Thread.Sleep(10); }
+            Assert.That(messg.Equals(JsonConvert.SerializeObject(new { a = "bitch" })));
+        }
+
+
+    }
+}
