29347fb25e02ccd5e7ccfd7949afed1dd7103b4f
Jokinko <jozefchmelar@users.noreply.github.com>
2022-01-19 13:26:31 +0100

    #321 Add OnCreated, OnUpdated, OnDeleted, OnRead delegates in TcoDataRepository separate for successful and failed operation

--- src/TcOpen.Inxton/src/Abstractions/Data/IRepository.cs
+++ src/TcOpen.Inxton/src/Abstractions/Data/IRepository.cs
@@ -1,10 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TcOpen.Inxton.Data
 {
     public delegate void OnCreateDelegate<T>(string id, T data);
+    public delegate void OnReadDelegate(string id);
     public delegate void OnUpdateDelegate<T>(string id, T data);
+    public delegate void OnDeleteDelegate(string id);
+
+    public delegate void OnCreateDoneDelegate<T>(string id, T data);
+    public delegate void OnReadDoneDelegate<T>(string id, T data);
+    public delegate void OnUpdateDoneDelegate<T>(string id, T data);
+    public delegate void OnDeleteDoneDelegate(string id);
+
+    public delegate void OnCreateFailedDelegate<T>(string id, T data, Exception ex);
+    public delegate void OnReadFailedDelegate(string id, Exception ex);
+    public delegate void OnUpdateFailedDelegate<T>(string id, T data, Exception ex);
+    public delegate void OnDeleteFailedDelegate(string id, Exception ex);
+
     public delegate IEnumerable<DataItemValidation> ValidateDataDelegate<T>(T data);
 
     public interface IRepository
@@ -17,7 +31,7 @@ namespace TcOpen.Inxton.Data
         dynamic Read(string identifier);
         void Update(string identifier, object data);
     }
-       
+
     public interface IRepository<T> where T : IBrowsableDataObject
     {
         long Count { get; }
@@ -30,7 +44,17 @@ namespace TcOpen.Inxton.Data
         T Read(string identifier);
         void Update(string identifier, T data);
         OnCreateDelegate<T> OnCreate { get; set; }
+        OnReadDelegate OnRead { get; set; }
         OnUpdateDelegate<T> OnUpdate { get; set; }
+        OnDeleteDelegate OnDelete { get; set; }
+        OnCreateDoneDelegate<T> OnCreateDone { get; set; }
+        OnReadDoneDelegate<T> OnReadDone { get; set; }
+        OnUpdateDoneDelegate<T> OnUpdateDone { get; set; }
+        OnDeleteDoneDelegate OnDeleteDone { get; set; }
+        OnCreateFailedDelegate<T> OnCreateFailed { get; set; }
+        OnReadFailedDelegate OnReadFailed { get; set; }
+        OnUpdateFailedDelegate<T> OnUpdateFailed { get; set; }
+        OnDeleteFailedDelegate OnDeleteFailed { get; set; }
         ValidateDataDelegate<T> OnRecordUpdateValidation { get; set; }
     }
 }
