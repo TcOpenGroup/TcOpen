7921dbd85c12633d4f7f37264f5eb5ff8f72565c
PTKu <61538034+PTKu@users.noreply.github.com>
2022-02-15 07:16:01 +0100

    wip

---
+++ src/TcoInspectors/src/TcoInspectorsConnector/pex/Abstractions/TcoInspectorExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using TcoCore;
+
+namespace TcoInspectors
+{
+    public static class TcoInspectorExtensions
+    {
+        public static IEnumerable<TcoInspector> ServeInspectors(this TcoObject obj, Action action)
+        {
+            var instpectors = obj.GetDescendants<TcoInspector>();
+
+            foreach (var inspector in instpectors)
+            {
+                inspector._dialogueTask.Initialize(action);
+            }
+
+            return instpectors;
+        }
+    }
+}
