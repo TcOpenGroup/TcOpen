64e3058fc2f2ac37b3fe887e228bcf61b1e58eb9
TomKovac <61820360+TomKovac@users.noreply.github.com>
2022-03-18 09:19:43 +0100

    sync

--- src/TcoIoBeckhoff/tests/PlcUnitTests/UnitTest1.cs
+++ src/TcoIoBeckhoff/tests/PlcUnitTests/UnitTest1.cs
@@ -7,39 +7,39 @@ namespace TcoIoBeckhoffUnitTests
 {
     public class fbUnitTestExampleTests
     {
-        SampleComponentTests sut;
+        //SampleComponentTests sut;
 
-        [OneTimeSetUp]
-        public void Setup()
-        {
-            Entry.TcoIoBeckhoffTests.Connector.BuildAndStart().ReadWriteCycleDelay = 100;
-            sut = Entry.TcoIoBeckhoffTests.MAIN._sampleComponentTests;
-        }
+        //[OneTimeSetUp]
+        //public void Setup()
+        //{
+        //    Entry.TcoIoBeckhoffTests.Connector.BuildAndStart().ReadWriteCycleDelay = 100;
+        //    sut = Entry.TcoIoBeckhoffTests.MAIN._sampleComponentTests;
+        //}
 
-        [Test]
-        [TestCase(10, 42)]
-        [TestCase(50, 42)]
-        public void AddTwoNumbersTest(short number1, short number2)
-        {
-            //-- Arrange
-            var expected = number1 + number2;
+        //[Test]
+        //[TestCase(10, 42)]
+        //[TestCase(50, 42)]
+        //public void AddTwoNumbersTest(short number1, short number2)
+        //{
+        //    //-- Arrange
+        //    var expected = number1 + number2;
 
-            //-- Act
-            var actual = sut.Run(p => sut.AddTwoNumbersTest(number1, number2), 1);
+        //    //-- Act
+        //    var actual = sut.Run(p => sut.AddTwoNumbersTest(number1, number2), 1);
 
 
-            //-- Assert
-            Assert.AreEqual(expected, actual);
-        }
+        //    //-- Assert
+        //    Assert.AreEqual(expected, actual);
+        //}
 
-        [Test]
-        [TestCase(10)]
-        [TestCase(50)]
-        [Timeout(1000)]
-        public void WaitForModuloTest(short modulo)
-        {            
-            //-- Act            
-            sut.Run(a => sut.WaitForModuloTest(modulo));            
-        }
+        //[Test]
+        //[TestCase(10)]
+        //[TestCase(50)]
+        //[Timeout(1000)]
+        //public void WaitForModuloTest(short modulo)
+        //{            
+        //    //-- Act            
+        //    sut.Run(a => sut.WaitForModuloTest(modulo));            
+        //}
     }
 }
