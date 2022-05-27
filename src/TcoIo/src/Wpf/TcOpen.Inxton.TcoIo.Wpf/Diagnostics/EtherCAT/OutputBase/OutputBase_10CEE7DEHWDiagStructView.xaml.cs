97dfff4b2435b309e17191ab9e0c37af7acb5a36
TomKovac <61820360+TomKovac@users.noreply.github.com>
2022-05-27 16:44:39 +0200

    OutputBaseStruct added

---
+++ src/TcoIo/src/Wpf/TcOpen.Inxton.TcoIo.Wpf/Diagnostics/EtherCAT/OutputBase/OutputBase_10CEE7DEHWDiagStructView.xaml.cs
@@ -0,0 +1,14 @@
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace TcoIo
+{
+    public partial class OutputBase_10CEE7DEHWDiagStructView : UserControl
+    {
+        public OutputBase_10CEE7DEHWDiagStructView()
+        {
+            InitializeComponent();
+        }
+    }
+}
