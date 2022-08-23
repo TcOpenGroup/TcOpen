56394310fb9d3b709dff08dfec7373c4b3b28348
Brano5 <97288158+Brano5@users.noreply.github.com>
2022-08-23 13:51:54 +0200

    update Startup

--- src/TcOpen.Hammer/PlcHammer.Hmi.Blazor/Startup.cs
+++ src/TcOpen.Hammer/PlcHammer.Hmi.Blazor/Startup.cs
@@ -14,7 +14,6 @@ using PlcHammer.Hmi.Blazor.Data;
 using PlcHammer.Hmi.Blazor.Security;
 using PlcHammer.Hmi.Blazor.Shared;
 using PlcHammerConnector;
-using Serilog;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -32,26 +31,17 @@ using TcOpen.Inxton.Local.Security.Blazor.Services;
 using TcOpen.Inxton.Local.Security.Blazor.Users;
 using TcOpen.Inxton.TcoCore.Blazor.Extensions;
 using Vortex.Presentation.Blazor.Services;
-using TcOpen.Inxton.Security;
 
 namespace PlcHammer.Hmi.Blazor
 {
     public class Startup
     {
-<<<<<<< HEAD
-        private BlazorRoleGroupManager roleGroupManager;
-        private bool mongoDB = true;
-
-=======
->>>>>>> dev
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
-
-        public static StringWriter logMessages = new StringWriter();
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -82,40 +72,7 @@ namespace PlcHammer.Hmi.Blazor
             roleGroupManager = new BlazorRoleGroupManager(groupRepo);
             Roles.Create(roleGroupManager);
 
-<<<<<<< HEAD
-            services.AddVortexBlazorSecurity(userRepo, groupRepo, roleGroupManager);
-
-            services.AddTcoCoreExtensions();
-            if (mongoDB)/*Mongo repositories for data*/
-            {
-                SetUpMongoDatabase();
-            }
-            else /*Json repositories for data*/
-            {
-                SetUpJsonRepositories();
-            }
-
-            // App setup
-            TcOpen.Inxton.TcoAppDomain.Current.Builder
-                .SetUpLogger(new TcOpen.Inxton.Logging.SerilogAdapter(new LoggerConfiguration()
-                                        //.WriteTo.TextWriter(logMessages)
-                                        .WriteTo.Console()        // This will write log into application console.  
-                                        .WriteTo.Notepad()        // This will write logs to first instance of notepad program.
-                                                                  // uncomment this to send logs over MQTT, to receive the data run MQTTTestClient from this solution.
-                                                                  // .WriteTo.MQTT(new MQTTnet.Client.Options.MqttClientOptionsBuilder().WithTcpServer("broker.emqx.io").Build(), "fun_with_TcOpen_Hammer") 
-                                        .Enrich.WithProperty("user",/*_signInManager.Context.User*/ TcOpen.Inxton.Local.Security.SecurityManager.Manager.Principal.Identity.Name)
-                                        .Enrich.With(new Serilog.Enrichers.EnvironmentNameEnricher())
-                                        .Enrich.With(new Serilog.Enrichers.EnvironmentUserNameEnricher())
-                                        .Enrich.With(new Serilog.Enrichers.MachineNameEnricher())
-                                        .MinimumLevel.Verbose())) // Sets the logger configuration (default reports only to console).
-                .SetSecurity(SecurityManager.Manager.Service)
-                .SetEditValueChangeLogging(Entry.PlcHammer.Connector);
-
-            // Initialize logger
-            Entry.PlcHammer.TECH_MAIN._app._logger.StartLoggingMessages(TcoCore.eMessageCategory.Info);
-=======
             services.AddVortexBlazorSecurity(userRepo, roleGroupManager);
->>>>>>> dev
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
