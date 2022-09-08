68c612eb339c0a251e6169fc835287ee743d4cae
TomKovac <61820360+TomKovac@users.noreply.github.com>
2022-09-08 18:56:56 +0200

    Add agent notifications (#514)
    * Update nightly.yml
    
    * Update main.yml
    
    * agent notification added, github secreats used
    
    * EtcGroupedView: added changing device list dimension acoordig to state
    
    Co-authored-by: Peter Kurhajec <61538034+PTKu@users.noreply.github.com>

---
+++ src/TcoIo/src/Wpf/TcOpen.Inxton.TcoIo.Wpf/Diagnostics/EtherCAT/Converters/InfoDataState/InfoDataStateToErrorState.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace TcoIo.Converters
+{
+    public class InfoDataStateToErrorState : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //0x001_ = Slave signals error
+            //0x002_ = Invalid vendorId, productCode... read
+            //0x004_ = Initialization error occurred
+            //0x008_ = Slave disabled
+            string ret = "";
+            if ((((int)(ushort)value) & 0x0010) != 0) ret = ret + "Error; ";
+            if ((((int)(ushort)value) & 0x0020) != 0) ret = ret + "Invalid productCode; ";
+            if ((((int)(ushort)value) & 0x0040) != 0) ret = ret + "Initialization error; ";
+            if ((((int)(ushort)value) & 0x0080) != 0) ret = ret + "Slave disabled; ";
+            return ret;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+    }
+
+}
---
+++ src/TcoIo/src/Wpf/TcOpen.Inxton.TcoIo.Wpf/Diagnostics/EtherCAT/Converters/RowsCountToResidualSpace.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace TcoIo.Converters
+{
+    public class RowsCountToResidualSpace : MarkupExtension, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string s = value as string;
+            string[] separators = { Environment.NewLine };
+            int rows = 0;
+            if (!string.IsNullOrEmpty(s))
+            {
+                rows = s.Split(separators, StringSplitOptions.RemoveEmptyEntries).Length;
+            }
+            return 100.0 - 11.0 * rows;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+    }
+}
