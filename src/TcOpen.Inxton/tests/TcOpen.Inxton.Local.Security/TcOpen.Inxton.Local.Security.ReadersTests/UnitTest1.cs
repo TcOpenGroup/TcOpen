48914e6d7e3bc449cbabbf232b5beca81a2964b5
Peter <61538034+PTKu@users.noreply.github.com>
2022-09-08 08:22:13 +0200

    added basic tests for the token providers

--- src/TcOpen.Inxton/tests/TcOpen.Inxton.Local.Security/TcOpen.Inxton.Local.Security.ReadersTests/UnitTest1.cs
+++ src/TcOpen.Inxton/tests/TcOpen.Inxton.Local.Security/TcOpen.Inxton.Local.Security.ReadersTests/UnitTest1.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System;
+using TcOpen.Inxton.Local.Security.Readers;
 
 namespace TcOpen.Inxton.Local.Security.ReadersTests
 {
@@ -10,9 +12,19 @@ namespace TcOpen.Inxton.Local.Security.ReadersTests
         }
 
         [Test]
-        public void Test1()
+        public void CreatePlcTokenReader()
         {
-            Assert.Pass();
+            var tokenValueSource = new Vortex.Connector.ValueTypes.OnlinerString();
+            var tokenPresence = new Vortex.Connector.ValueTypes.OnlinerBool();
+            var reader = ExternalTokenAuthorization.CreatePlcTokenReader(tokenValueSource, tokenPresence);            
+        }
+
+        [Test]
+        public void CreateComTokenReader()
+        {
+            var tokenValueSource = new Vortex.Connector.ValueTypes.OnlinerString();
+            var tokenPresence = new Vortex.Connector.ValueTypes.OnlinerBool();
+            Assert.Throws<System.IO.IOException>(() => ExternalTokenAuthorization.CreateComReader("COM1"));            
         }
     }
 }
