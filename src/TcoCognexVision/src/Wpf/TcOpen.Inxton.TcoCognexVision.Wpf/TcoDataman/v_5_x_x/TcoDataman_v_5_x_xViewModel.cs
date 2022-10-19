76ce5c76f6c1e20cef82fa5fbddba2fd2315a3fa
TomKovac <61820360+TomKovac@users.noreply.github.com>
2022-10-19 13:30:43 +0200

    TcoDataman PLC side

---
+++ src/TcoCognexVision/src/Wpf/TcOpen.Inxton.TcoCognexVision.Wpf/TcoDataman/v_5_x_x/TcoDataman_v_5_x_xViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vortex.Presentation.Wpf;
+
+namespace TcoCognexVision
+{
+   
+    public class TcoDataman_v_5_x_xViewModel : RenderableViewModel
+    {
+        public TcoDataman_v_5_x_xViewModel()
+        {
+           
+        }
+
+        public TcoDataman_v_5_x_x Component { get; private set; }
+
+        public override object Model { get => this.Component; set { this.Component = value as TcoDataman_v_5_x_x; } }
+    }
+
+    public class TcoDataman_v_5_x_xServiceViewModel : TcoDataman_v_5_x_xViewModel
+    {
+
+    }
+}
