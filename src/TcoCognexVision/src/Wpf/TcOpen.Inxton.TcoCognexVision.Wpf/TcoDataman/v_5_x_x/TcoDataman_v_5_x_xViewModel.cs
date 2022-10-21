9e15ab748c9df56e6e8729ac042660e73b78728f
TomKovac <61820360+TomKovac@users.noreply.github.com>
2022-10-21 21:27:00 +0200

    view of data inside header, but binding to everything else not working

--- src/TcoCognexVision/src/Wpf/TcOpen.Inxton.TcoCognexVision.Wpf/TcoDataman/v_5_x_x/TcoDataman_v_5_x_xViewModel.cs
+++ src/TcoCognexVision/src/Wpf/TcOpen.Inxton.TcoCognexVision.Wpf/TcoDataman/v_5_x_x/TcoDataman_v_5_x_xViewModel.cs
@@ -29,78 +29,108 @@ namespace TcoCognexVision
         public TcoDataman_v_5_x_xViewModel() : base()
         {
             ResultData = new ObservableCollection<IndexedData<string>>();
-            Model = new TcoDataman_v_5_x_x();
+            Model = new TcoDataman_v_5_x_xExtend();
         }
 
-        public TcoDataman_v_5_x_x Component { get; private set; }
+        public TcoDataman_v_5_x_xExtend Component { get; private set; }
 
         public override object Model 
         { 
             get => Component; 
             set 
             {
-                Component = value as TcoDataman_v_5_x_x;
-                UpdateAndFormatResultData();
-                Component._resultData.Length.Subscribe((sender, arg) => UpdateAndFormatResultData());
-                Component._resultData.Id.Subscribe((sender, arg) => UpdateAndFormatResultData());
+                if (value.GetType().Equals(typeof(TcoDataman_v_5_x_xExtend)))
+                {
+                    Component = value as TcoDataman_v_5_x_xExtend;
+                    UpdateAndFormatResultData();
+                    Component.Base._resultData.Length.Subscribe((sender, arg) => UpdateAndFormatResultData());
+                    Component.Base._resultData.Id.Subscribe((sender, arg) => UpdateAndFormatResultData());
+                }
+                else if (value.GetType().Equals(typeof(TcoDataman_v_5_x_x)))
+                {
+                    Component.Base = value as TcoDataman_v_5_x_x;
+                    Component.ResultData = ResultData;
+                    UpdateAndFormatResultData();
+                    Component.Base._resultData.Length.Subscribe((sender, arg) => UpdateAndFormatResultData());
+                    Component.Base._resultData.Id.Subscribe((sender, arg) => UpdateAndFormatResultData());
+                }
+
             }
         }
 
         private void UpdateAndFormatResultData()
         {
-            try
+            TcOpen.Inxton.TcoAppDomain.Current.Dispatcher.Invoke(() =>
             {
-                if (ResultData == null)
+                try
                 {
-                    ResultData = new ObservableCollection<IndexedData<string>>();
-                }
-                else
-                {
-                    ResultData.Clear();
-                }
-                if (Component != null && Component.GetConnector() != null && Component._resultData != null)
-                {
-                    Component._resultData.Read();
-                    if (CurrentDisplayFormat == eDisplayFormat.Array_of_decimals)
+                    if (ResultData == null)
                     {
-                        for (int i = 0; i < Component._resultData.Length.LastValue; i++)
+                        ResultData = new ObservableCollection<IndexedData<string>>();
+                    }
+                    else
+                    {
+                        ResultData.Clear();
+                    }
+                    if (Component != null && Component.Base.GetConnector() != null && Component.Base._resultData != null)
+                    {
+                        Component.Base._resultData.Read();
+                        if (CurrentDisplayFormat == eDisplayFormat.Array_of_decimals)
                         {
-                            ResultData.Add(new IndexedData<string>(i, Component._resultData.Data[i].LastValue.ToString()));
+                            for (int i = 0; i < Component.Base._resultData.Length.LastValue; i++)
+                            {
+                                ResultData.Add(new IndexedData<string>(i, Component.Base._resultData.Data[i].LastValue.ToString()));
+                            }
+                        }
+                        else if (CurrentDisplayFormat == eDisplayFormat.Array_of_hexdecimals)
+                        {
+                            for (int i = 0; i < Component.Base._resultData.Length.LastValue; i++)
+                            {
+                                ResultData.Add(new IndexedData<string>(i, Component.Base._resultData.Data[i].LastValue.ToString("X")));
+                            }
+                        }
+                        else if (CurrentDisplayFormat == eDisplayFormat.String)
+                        {
+                            for (int i = 0; i < Component.Base._resultData.Length.LastValue; i++)
+                            {
+                                byte _byte = Component.Base._resultData.Data[i].LastValue;
+                                string _string = "";
+                                if (_byte > 0)
+                                    _string = Encoding.UTF8.GetString(new byte[] { _byte });
+                                else _string = "N/A";
+
+                                ResultData.Add(new IndexedData<string>(i, _string));
+                            }
                         }
                     }
-                    else if (CurrentDisplayFormat == eDisplayFormat.Array_of_hexdecimals)
-                    {
-                        for (int i = 0; i < Component._resultData.Length.LastValue; i++)
-                        {
-                            ResultData.Add(new IndexedData<string>(i, Component._resultData.Data[i].LastValue.ToString("X")));
-                        }
-                    }
-                    else if (CurrentDisplayFormat == eDisplayFormat.String)
-                    {
-                        for (int i = 0; i < Component._resultData.Length.LastValue; i++)
-                        {
-                            byte _byte = Component._resultData.Data[i].LastValue;
-                            string _string = "";
-                            if (_byte > 0)
-                                _string = Encoding.UTF8.GetString(new byte[] { _byte });
-                            else _string = "N/A";
-
-                            ResultData.Add(new IndexedData<string>(i, _string));
-                        }
-                    }
                 }
-            }
-            catch (System.Exception ex)
-            {
+                catch (System.Exception ex)
+                {
 
-                throw;
-            }
-
-
+                    throw;
+                }
+            });
         }
 
     }
 
+    public class TcoDataman_v_5_x_xExtend
+    {
+        public TcoDataman_v_5_x_x Base { get; set; }
+        public ObservableCollection<IndexedData<string>> ResultData { get; set; }
+
+        public TcoDataman_v_5_x_xExtend(TcoDataman_v_5_x_x _base, ObservableCollection<IndexedData<string>> _resultData)
+        {
+            Base = _base;
+            ResultData = _resultData;
+        }
+        public TcoDataman_v_5_x_xExtend()
+        {
+            Base = new TcoDataman_v_5_x_x();
+            ResultData = new ObservableCollection<IndexedData<string>>();
+        }
+
+    }
     public class TcoDataman_v_5_x_xServiceViewModel : TcoDataman_v_5_x_xViewModel
     {
 
