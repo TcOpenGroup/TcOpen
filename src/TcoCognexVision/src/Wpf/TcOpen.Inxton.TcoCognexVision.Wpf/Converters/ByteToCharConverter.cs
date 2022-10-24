e1dc80359deeb031fdd915ccae6eefd9438a2f2f
TomKovac <61820360+TomKovac@users.noreply.github.com>
2022-10-24 12:34:52 +0200

    dataman first functional view

--- src/TcoCognexVision/src/Wpf/TcOpen.Inxton.TcoCognexVision.Wpf/Converters/ByteToCharConverter.cs
+++ src/TcoCognexVision/src/Wpf/TcOpen.Inxton.TcoCognexVision.Wpf/Converters/ByteToCharConverter.cs
@@ -5,20 +5,20 @@ using Vortex.Presentation.Wpf.Converters;
 
 namespace TcoCognexVision.Converters
 {
-    public class ByteToCharConverter : BaseConverter
-    {
-        public override object ToConvert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (value is byte)
-            {
-                if ((byte)value > 0)
-                    return Encoding.UTF8.GetString(new byte[] { (byte)value });
-                else return "N/A";
-            }
-            else
-            {
-                return "N/A";
-            }
-        }
-    }
+    //public class ByteToCharConverter : BaseConverter
+    //{
+    //    public override object ToConvert(object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        if (value is byte)
+    //        {
+    //            if ((byte)value > 0)
+    //                return Encoding.UTF8.GetString(new byte[] { (byte)value });
+    //            else return "N/A";
+    //        }
+    //        else
+    //        {
+    //            return "N/A";
+    //        }
+    //    }
+    //}
 }
--- src/TcoCognexVision/src/Wpf/TcOpen.Inxton.TcoCognexVision.Wpf/TcoDataman/v_5_x_x/TcoDataman_v_5_x_xViewModel.cs
+++ src/TcoCognexVision/src/Wpf/TcOpen.Inxton.TcoCognexVision.Wpf/TcoDataman/v_5_x_x/TcoDataman_v_5_x_xViewModel.cs
@@ -1,19 +1,14 @@
 ﻿using System.Collections.ObjectModel;
-using Vortex.Connector.ValueTypes;
 using Vortex.Presentation.Wpf;
 using TcoCognexVision.Converters;
-using System.Linq;
-using TcoCore;
 using Vortex.Connector;
 using System.Text;
 
 namespace TcoCognexVision
 {
-
     public class TcoDataman_v_5_x_xViewModel : RenderableViewModel
     {
         private eDisplayFormat _currentDisplayFormat;
-
         public eDisplayFormat CurrentDisplayFormat
         {
             get => _currentDisplayFormat;
@@ -23,41 +18,24 @@ namespace TcoCognexVision
                 UpdateAndFormatResultData();
             }
         }
-
         public ObservableCollection<IndexedData<string>> ResultData { get ; private set; }
-
         public TcoDataman_v_5_x_xViewModel() : base()
         {
             ResultData = new ObservableCollection<IndexedData<string>>();
-            Model = new TcoDataman_v_5_x_xExtend();
+            Model = new TcoDataman_v_5_x_x();
         }
-
-        public TcoDataman_v_5_x_xExtend Component { get; private set; }
-
+        public TcoDataman_v_5_x_x Component { get; private set; }
         public override object Model 
         { 
             get => Component; 
             set 
             {
-                if (value.GetType().Equals(typeof(TcoDataman_v_5_x_xExtend)))
-                {
-                    Component = value as TcoDataman_v_5_x_xExtend;
-                    UpdateAndFormatResultData();
-                    Component.Base._resultData.Length.Subscribe((sender, arg) => UpdateAndFormatResultData());
-                    Component.Base._resultData.Id.Subscribe((sender, arg) => UpdateAndFormatResultData());
-                }
-                else if (value.GetType().Equals(typeof(TcoDataman_v_5_x_x)))
-                {
-                    Component.Base = value as TcoDataman_v_5_x_x;
-                    Component.ResultData = ResultData;
-                    UpdateAndFormatResultData();
-                    Component.Base._resultData.Length.Subscribe((sender, arg) => UpdateAndFormatResultData());
-                    Component.Base._resultData.Id.Subscribe((sender, arg) => UpdateAndFormatResultData());
-                }
-
+                Component = value as TcoDataman_v_5_x_x ;
+                UpdateAndFormatResultData();
+                Component._resultData.Length.Subscribe((sender, arg) => UpdateAndFormatResultData());
+                Component._resultData.Id.Subscribe((sender, arg) => UpdateAndFormatResultData());
             }
         }
-
         private void UpdateAndFormatResultData()
         {
             TcOpen.Inxton.TcoAppDomain.Current.Dispatcher.Invoke(() =>
@@ -72,28 +50,28 @@ namespace TcoCognexVision
                     {
                         ResultData.Clear();
                     }
-                    if (Component != null && Component.Base.GetConnector() != null && Component.Base._resultData != null)
+                    if (Component != null && Component.GetConnector() != null && Component._resultData != null)
                     {
-                        Component.Base._resultData.Read();
+                        Component._resultData.Read();
                         if (CurrentDisplayFormat == eDisplayFormat.Array_of_decimals)
                         {
-                            for (int i = 0; i < Component.Base._resultData.Length.LastValue; i++)
+                            for (int i = 0; i < Component._resultData.Length.LastValue; i++)
                             {
-                                ResultData.Add(new IndexedData<string>(i, Component.Base._resultData.Data[i].LastValue.ToString()));
+                                ResultData.Add(new IndexedData<string>(i, Component._resultData.Data[i].LastValue.ToString()));
                             }
                         }
                         else if (CurrentDisplayFormat == eDisplayFormat.Array_of_hexdecimals)
                         {
-                            for (int i = 0; i < Component.Base._resultData.Length.LastValue; i++)
+                            for (int i = 0; i < Component._resultData.Length.LastValue; i++)
                             {
-                                ResultData.Add(new IndexedData<string>(i, Component.Base._resultData.Data[i].LastValue.ToString("X")));
+                                ResultData.Add(new IndexedData<string>(i, Component._resultData.Data[i].LastValue.ToString("X")));
                             }
                         }
                         else if (CurrentDisplayFormat == eDisplayFormat.String)
                         {
-                            for (int i = 0; i < Component.Base._resultData.Length.LastValue; i++)
+                            for (int i = 0; i < Component._resultData.Length.LastValue; i++)
                             {
-                                byte _byte = Component.Base._resultData.Data[i].LastValue;
+                                byte _byte = Component._resultData.Data[i].LastValue;
                                 string _string = "";
                                 if (_byte > 0)
                                     _string = Encoding.UTF8.GetString(new byte[] { _byte });
@@ -111,25 +89,6 @@ namespace TcoCognexVision
                 }
             });
         }
-
-    }
-
-    public class TcoDataman_v_5_x_xExtend
-    {
-        public TcoDataman_v_5_x_x Base { get; set; }
-        public ObservableCollection<IndexedData<string>> ResultData { get; set; }
-
-        public TcoDataman_v_5_x_xExtend(TcoDataman_v_5_x_x _base, ObservableCollection<IndexedData<string>> _resultData)
-        {
-            Base = _base;
-            ResultData = _resultData;
-        }
-        public TcoDataman_v_5_x_xExtend()
-        {
-            Base = new TcoDataman_v_5_x_x();
-            ResultData = new ObservableCollection<IndexedData<string>>();
-        }
-
     }
     public class TcoDataman_v_5_x_xServiceViewModel : TcoDataman_v_5_x_xViewModel
     {
