f8edb95a962c387b8280bb3dea5f3c6918ca43cd
Peter Barancek <peter.barancek@mts.sk>
2022-10-25 14:51:46 +0200

    *TcoComponetView binding model canged *re-arange layout in TcoSimpleDrive View +fix color styles sandbox projects + inTcoElements added GenericViewModel (if we dont have a model) and change context in code behind *TcoCylinder layout

--- src/TcoElements/src/Wpf/TcoElements.Wpf/Digital/TcoDi/Service/TcoDiServiceView.xaml.cs
+++ src/TcoElements/src/Wpf/TcoElements.Wpf/Digital/TcoDi/Service/TcoDiServiceView.xaml.cs
@@ -1,5 +1,8 @@
 using System.ComponentModel;
+using System.Windows;
 using System.Windows.Controls;
+using TcOpen.Inxton.Wpf;
+using Vortex.Presentation.Wpf;
 
 namespace TcoElements
 {
@@ -16,7 +19,17 @@ namespace TcoElements
             }
 
             InitializeComponent();
+        }
 
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+
+            if (e.Property == DataContextProperty)
+            {
+                this.DataContext = this.DataContext.ViewModelizeDataContext<GenericViewModel, TcoDi>();
+            }
         }
     }
+
 }
