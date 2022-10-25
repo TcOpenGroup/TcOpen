d732e35b683c57be1c7646c517013a50f255a4f1
TomKovac <61820360+TomKovac@users.noreply.github.com>
2022-10-25 20:36:52 +0200

    readme added

--- src/TcoCognexVision/tests/Sandbox.TcoCognexVision.Wpf/MainWindowViewModel.cs
+++ src/TcoCognexVision/tests/Sandbox.TcoCognexVision.Wpf/MainWindowViewModel.cs
@@ -1,13 +1,7 @@
 using TcoCognexVisionTests;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using Vortex.Connector;
-using Vortex.Connector.ValueTypes;
-using System.Linq;
 
 namespace Sandbox.TcoCognexVision.Wpf
-{    
+{
     public class MainWindowViewModel 
     {
         public MainWindowViewModel()
