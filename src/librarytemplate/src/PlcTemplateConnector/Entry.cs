eab456a2e177f347c2655ba8b8a395ba15f167eb
TomKovac <61820360+TomKovac@users.noreply.github.com>
2022-10-26 10:45:28 +0200

    PlcTemplate library modifycation

--- src/librarytemplate/src/PlcTemplateConnector/Entry.cs
+++ src/librarytemplate/src/PlcTemplateConnector/Entry.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vortex.Adapters.Connector.Tc3.Adapter;
 
 namespace PlcTemplate
 {
     public static class Entry
     {
-        const string AmdId = "172.20.10.2.1.1";
+        static string TargetAmsId = Environment.GetEnvironmentVariable("Tc3Target");
+        static int TargetAmsPort = 851;
 
         public static PlcTemplate.PlcTemplateTwinController PlcTemplatePlc 
-            = new PlcTemplateTwinController(Tc3ConnectorAdapter.Create(AmdId, 851, true));
+            = new PlcTemplateTwinController(Tc3ConnectorAdapter.Create(TargetAmsId, TargetAmsPort, true));
     }
 }
--- src/librarytemplate/tests/PlcTemplateTestsConnector/Entry.cs
+++ src/librarytemplate/tests/PlcTemplateTestsConnector/Entry.cs
@@ -1,17 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vortex.Adapters.Connector.Tc3.Adapter;
 
 namespace PlcTemplateTests
 {
     public static class Entry
     {
-        const string AmdId = "172.20.10.2.1.1";
+        static string TargetAmsId = Environment.GetEnvironmentVariable("Tc3Target");
+        static int TargetAmsPort = 852;
 
         public static PlcTemplateTestsTwinController PlcTemplateTests 
-            = new PlcTemplateTestsTwinController(Tc3ConnectorAdapter.Create(AmdId, 852, true));
+            = new PlcTemplateTestsTwinController(Tc3ConnectorAdapter.Create(TargetAmsId, TargetAmsPort, true));
     }
 }
