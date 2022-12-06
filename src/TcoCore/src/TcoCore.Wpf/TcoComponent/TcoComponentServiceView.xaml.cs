ada4f9ebaa04385251516ee274b773673ab48e6c
peterbarancek <62284244+peterbarancek@users.noreply.github.com>
2022-12-06 10:06:10 +0100

    *fix header layout  in TcoComponentView (#561)
    * change concept for TcoComponentServiceView , not based on TcoComponentView  anymore

--- src/TcoCore/src/TcoCore.Wpf/TcoComponent/TcoComponentServiceView.xaml.cs
+++ src/TcoCore/src/TcoCore.Wpf/TcoComponent/TcoComponentServiceView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Vortex.Presentation.Wpf;
 
 namespace TcoCore
 {
@@ -25,4 +26,6 @@ namespace TcoCore
             InitializeComponent();
         }
     }
+
+   
 }
