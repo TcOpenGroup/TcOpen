25b3c01dc98165b937ff459e5af0462bdd95622d
peterbarancek <62284244+peterbarancek@users.noreply.github.com>
2023-12-05 09:30:37 +0100

    Fix single axis move param (#667)
    * Create draft PR for #665
    
    * *fixed  viewmodel single axis
    
    ---------
    
    Co-authored-by: peterbarancek <peterbarancek@users.noreply.github.com>

--- src/TcoDrivesBeckhoff/src/TcoDrivesBeckhoff.Wpf/TcoSingleAxisPositions/TcoSingleAxisPositionsRootContainerViewModel.cs
+++ src/TcoDrivesBeckhoff/src/TcoDrivesBeckhoff.Wpf/TcoSingleAxisPositions/TcoSingleAxisPositionsRootContainerViewModel.cs
@@ -19,7 +19,7 @@ namespace TcoDrivesBeckhoff
 
         public TcoSingleAxisPositionsRootContainer Component { get; private set; }
 
-        public ObservableCollection<TcoMultiAxisMoveParam> Positions { get { return Extensions.ToObservableCollection<TcoMultiAxisMoveParam>(((IVortexObject)Model).GetChildren().OfType<TcoMultiAxisMoveParam>()); } }
+        public ObservableCollection<TcoSingleAxisMoveParam> Positions { get { return Extensions.ToObservableCollection<TcoSingleAxisMoveParam>(((IVortexObject)Model).GetChildren().OfType<TcoSingleAxisMoveParam>()); } }
         public TcoSingleAxisMoveParam SelectedItem { get; set; }
 
 
